f36dd8d849c991d9a70356a7fc32e2e2190ee4ff
daqiao <yihqiao@126.com>
2021-07-19 20:35:11 +0800

    Add ArithmeticOperation

---
+++ introduction/operation/src/ArithmeticOperation.java
@@ -0,0 +1,19 @@
+/**
+ * @author: qiaoyihan
+ * @date: 2021/7/19
+ */
+public class ArithmeticOperation {
+
+    public static void main(String[] args) {
+        int a = 45;
+        int b = 160;
+
+        int c = a + b;
+        System.out.println("45 + 160 = " + c);
+        c = a - b;
+        System.out.println("45 - 160 = " + c);
+
+
+
+    }
+}
